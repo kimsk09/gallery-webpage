e18b2abdbccb15cec652d881f25918d6d46275d3
kimsk09 <arken1256@gmail.com>
2024-09-13 09:43:05 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+$(".btns>li").click(function(){
+ $(this).addClass("on")
+ $(this).siblings().removeClass("on")
+ //속성변경시-->$(선택자).attr('속성명','속성값');
+  //속성값 호출시(변수안에 넣어줌))-->a=$(선택자).attr('속성명');
+ let a=$(this).attr('data-filter'); 
+ $(".contents").isotope({filter:a});
+
+})
+
+$(".contents").isotope();
+$(".fancybox").fancybox();
+
